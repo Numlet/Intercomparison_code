14176da27f9a6dd8441caeb2c905b9105549d35b
Jesus Vergara Temprado <jesusvergaratemprado@Jesuss-MacBook-Pro.local>
2017-07-07 17:37:58 -0700

    Initial files

---
+++ create_csv.py
@@ -0,0 +1,86 @@
+import numpy as np
+import os
+os.chdir('/Users/jesusvergaratemprado/Intercomparison_code')
+import station_properties as stp
+from scipy.io import netcdf
+import pandas as pd
+def find_nearest_vector_index(array, value):
+    n = np.array([abs(i-value) for i in array])
+    nindex=np.apply_along_axis(np.argmin,0,n)
+    return nindex
+
+factor_OC2OM=1.9
+
+nc_path='/Users/jesusvergaratemprado/OLD_GLOMAP_NC/'
+
+total_TOA_file="total_organic_mass.nc"
+total_MOA_file='wiom_acc.nc'
+total_aerosol_file='total_aerosol_mass.nc'
+
+mb=netcdf.netcdf_file(nc_path+total_TOA_file,'r')
+mb.variables
+total_TOA=mb.variables['total_organic_mass'][:]
+mb=netcdf.netcdf_file(nc_path+total_MOA_file,'r')
+mb.variables
+total_MOA=mb.variables['wiom_acc'][:]
+mb=netcdf.netcdf_file(nc_path+total_aerosol_file,'r')
+mb.variables
+total_AM=mb.variables['total_aerosol_mass'][:]
+
+lat=mb.variables['lat'][:]
+levels=mb.variables['levels'][:]
+lon=mb.variables['lon'][:]
+lon180=np.copy(lon)
+lon180[lon>180]=lon[lon>180]-360
+
+months=range(1,13)
+
+
+from collections import OrderedDict
+
+
+d=OrderedDict()
+d['model']=[]
+d['OA']=[]
+d['OA type']=[]
+d['OC']=[]
+d['OC type']=[]
+d['aerosol']=[]
+d['aerosol type']=[]
+d['month']=[]
+d['lat']=[]
+d['lon']=[]
+
+iobs=54
+isurf=30
+types=["TOA","MOA"]
+for iobs in range(len(stp.lons)):
+    for imonth in months:
+
+        for aer_type in types:
+            # print iobs
+            lat_point=stp.lats[iobs]
+            lon_point=stp.lons[iobs]
+
+            ilat=find_nearest_vector_index(lat,lat_point)
+            if lon_point<0:
+                ilon=find_nearest_vector_index(lon180,lon_point)
+            else:
+                ilon=find_nearest_vector_index(lon,lon_point)
+
+            if aer_type is "MOA":data=total_MOA[isurf,ilat,ilon,imonth-1]
+            if aer_type is "TOA":data=total_TOA[isurf,ilat,ilon,imonth-1]
+            d['model'].append('GLOMAP')
+            d['lat'].append(lat_point)
+            d['lon'].append(lon_point)
+            d['OA'].append(data)
+            d['OA type'].append(aer_type)
+            d['OC'].append(data/factor_OC2OM)
+            d['OC type'].append(aer_type[:-1]+'C')
+            d['aerosol'].append(data)
+            d['aerosol type'].append(aer_type)
+            d['month']=imonth
+
+df=pd.DataFrame(d)
+
+df.to_csv('formated_data.csv')
---
+++ station_properties.py
@@ -0,0 +1,41 @@
+'''
+
+Properties of the measurement stations
+
+'''
+import numpy as np
+import os
+
+data=np.genfromtxt('MarineOM.csv',delimiter=',',dtype=None)
+
+print data
+
+lons=[360.0-11.25, 11.25, 56.25, 360.0-63.75, 78.75,
+                   360.0-88.75, 360.0-96.25, 111.25, 121.25, 128.75,
+                   158.75, 161.25, 171.25, 9.904, 360.0-122.91,
+                   360.0-20.30, 360.0-80.25, 360.0-59.43, 360.0-60,
+                   18.48, 306.0-64.05, 144.68, 62.50, 360.0-67.4, 360.0-64.7,
+                   360.-130., 360.0-62.5, 360.0-62.953, 25.67, 360.0-27.35]
+                #    filter_samples$Longitude)
+lats=[41., -19., -21., 33., -37., 27., 25.,
+                   -41., 7., 33., 37., 21., -23., 53.326, 38.12,
+                   63.40, 25.75, 13.17, -51.75,
+                  -34.35, -64.77, -40.68, -67.60, 41.9, 36.3,
+                  10., 82.46, 37.88261, 35.33, 38.]
+names=["west of Portugal", "west of Namibia", "La Reunion Island",
+                   "Bermuda", "Amsterdam Island", "Gulf of Mexico (north)",
+                   "Gulf of Mexico (west)", "southwest of Australia",
+                   "Philippines", "south of South Korea", "North Pacific Ocean 1",
+                   "North Pacific Ocean 2", "New Caledonia", "Mace Head", "Point Reyes",
+                   "Heimaey, Iceland", "Miami, Florida", "Ragged Point; Barbados",
+                   "Falkland Islands", "Cape Point, South Africa", "Palmer Station, Antarctica",
+                   "Cape Grim, Tasmania", "Mawson - Antarctica",
+                   "WACS-I - George's Bank", "WACS-I - Sargasso Sea",
+                   "SPURS-2", "Alert", "WACS-II", "Finokalia", "Azores"]
+
+i=2
+for i in range(data.shape[0]-1):
+    lons.append(float(data[i+1,19]))
+    lats.append(float(data[i+1,18]))
+    names.append(data[i+1,0])
+#                    as.character(filter_samples$FilterID))
