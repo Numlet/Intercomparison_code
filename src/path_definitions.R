f7d0be3a2db2f4304cb0db710beba2e174966883
Susannah Burrows <susannah.burrows@pnnl.gov>
2017-07-10 13:52:59 -0700

    Add a version of R path definitions script

---
+++ src/path_definitions.R
@@ -0,0 +1,19 @@
+# Directories with obs, model data, etc.
+
+# netCDF data location
+ncfile_dir <- "../../"
+ncfile_name <- "reduced_chem_MOA_code.FC5PLMOD.ne30_ne30.edison.MOA_mix3.nc"
+
+# Output file location
+csv_dir <- "../station_data_frames/ACMEv0-OCEANFILMS_mix3"
+csv_name_root <- "ACMEv0-OCEANFILMS_mix3"
+
+# Observations file path
+obs_dir <- "../obs_data/"
+
+# Graphics output directory
+plotdir="./graphics"
+
+# Define month names
+monthnames <- c("JAN", "FEB", "MAR", "APR", "MAY", "JUN",
+                "JUL", "AUG", "SEP", "OCT", "NOV", "DEC")
