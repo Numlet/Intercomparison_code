e749cdb21d2de9652e03b998cee2446a07809018
Jesus Vergara-Temprado <eejvt@leeds.ac.uk>
2017-07-13 15:56:01 -0700

    small change

--- create_csv_GLOMAP2.py
+++ create_csv_GLOMAP2.py
@@ -103,41 +103,7 @@ d['station']=[]
 d['lat']=[]
 d['lon']=[]
 
-iobs=54
 isurf=30
-types=["TOA","MOA"]
-# for iobs in range(len(stp.lons)):
-#     for imonth in months:
-#
-#         for aer_type in types:
-#             # print iobs
-#             lat_point=stp.lats[iobs]
-#             lon_point=stp.lons[iobs]
-#
-#             ilat=find_nearest_vector_index(lat,lat_point)
-#             if lon_point<0:
-#                 ilon=find_nearest_vector_index(lon180,lon_point)
-#             else:
-#                 ilon=find_nearest_vector_index(lon,lon_point)
-#
-#             if aer_type is "MOA":data=total_MOA[isurf,ilat,ilon,imonth-1]
-#             if aer_type is "TOA":data=total_TOA[isurf,ilat,ilon,imonth-1]
-#             d['model'].append('GLOMAP')
-#             d['lat'].append(lat_point)
-#             d['lon'].append(lon_point)
-#             d['OA'].append(data*1e3)#ng/m3
-#             d['OAtype'].append(aer_type)
-#             d['OC'].append(data/factor_OC2OM*1e3)#ng/m3
-#             d['OCtype'].append(aer_type[:-1]+'C')
-#             d['aerosol'].append(data)
-#             d['aerosoltype'].append(aer_type)
-#             d['month'].append(imonth)
-#             d['station'].append(stp.names[iobs])
-#
-# df=pd.DataFrame(d)
-#
-# df.to_csv('model_data/GLOMAP2_TOA_station.csv',mode = 'w', index=False)
-
 
 #%%
 months_str=["JAN","FEB","MAR","APR","MAY","JUN","JUL","AUG","SEP","OCT","NOV","DEC"]
